067326a9efce64b53c573f9d10a643a91fe455ca
Marek Baranowski <marek.baranowski@student.po.edu.pl>
2021-08-29 17:27:51 +0200

    feature: dodanie svg minify

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,50 +1,85 @@
-const {resolve} = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const {resolve} = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.tsx",
-  mode: "development",
-  output: {
-    filename: "main.js",
-    path: resolve(__dirname, "static"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-typescript'],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-            MiniCssExtractPlugin.loader,
-          "css-loader",
+    entry: './src/index.tsx',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: resolve(__dirname, 'static'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-typescript'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                loader: 'file-loader',
+            },
         ],
-      },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        loader: 'file-loader',
-      },
+    },
+    resolve: {
+        extensions: [
+            '.tsx',
+            '.ts',
+            '.js',
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+                plugins: [
+                    ['gifsicle', {interlaced: true,},],
+                    ['jpegtran', {progressive: true,},],
+                    ['optipng', {optimizationLevel: 5,},],
+                    [
+                        'svgo',
+                        {
+                            name: 'preset-default',
+                            params: {
+                                overrides: {
+                                    builtinPluginName: {
+                                        optionName: 'optionValue',
+                                    },
+                                    anotherBuiltinPlugin: false,
+                                },
+                            },
+                        },
+                        'moreBuiltinPlugin',
+                        {
+                            name: 'manyBuiltInPlugin',
+                            params: {
+                                optionName: 'value',
+                            },
+                        },
+                    ],
+                ],
+            },
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-      new MiniCssExtractPlugin({
-        filename: "style.css",
-      }),
-  ],
-  optimization: {
-    minimizer: [
-      new CssMinimizerPlugin(),
-    ],
-  },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+    },
 };
