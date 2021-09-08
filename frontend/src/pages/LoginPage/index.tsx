56cdf641edb0d855c9bbf0da9ff523af73ada260
Marek Baranowski <marek.baranowski@student.po.edu.pl>
2021-09-08 19:30:48 +0200

    feature: blokada wejścia na strone logowania zalogowanego użytkownika

--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -1,18 +1,29 @@
-import React, {useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import {Redirect} from 'react-router-dom';
 
 import LoginForm from '../../containers/LoginForm';
 import LoginOptions from '../../containers/LoginOptions';
 import SignInPerks from '../../components/SignInPerks';
+import {CurrentUserContext} from '../../context/CurrentUserContext';
 
 interface LoginPageProps {
 
 }
 
 const LoginPage: React.FC<LoginPageProps> = ({}) => {
+    const [redirect, setRedirect] = useState<boolean>(false);
     const [chooseLoginEmail, setChooseLoginEmail] = useState<boolean>(false);
+    const userContext = useContext(CurrentUserContext);
+
+    useEffect(() => {
+        userContext?.checkIsUserLogged().then(() => setRedirect(true), () => {});
+    }, []);
 
     return (
       <div className="wrapper-form">
+          {redirect && <Redirect to={{
+              pathname: '/',
+          }}/>}
           <div className="container-form">
               <h2>Zaloguj się</h2>
               {chooseLoginEmail ?
