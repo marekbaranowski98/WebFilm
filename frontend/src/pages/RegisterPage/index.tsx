92bedf04c46f1824261bf2a192526e503aad447a
Marek Baranowski <marek.baranowski@student.po.edu.pl>
2021-09-19 14:59:57 +0200

    fix: blokada wejścia na strone rejestracji gdy jest się zalogowanym

--- frontend/src/pages/RegisterPage/index.tsx
+++ frontend/src/pages/RegisterPage/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 
 import RegisterForm from '../../containers/RegisterForm';
+import {CurrentUserContext} from "../../context/CurrentUserContext";
+import {Redirect} from "react-router-dom";
 
 interface RegisterPageProps {
 
 }
 
 const RegisterPage: React.FC<RegisterPageProps> = ({}) => {
+    const [redirect, setRedirect] = useState<boolean>(false);
+    const userContext = useContext(CurrentUserContext);
+
+    useEffect(() => {
+        userContext?.checkIsUserLogged().then(() => setRedirect(true), () => {});
+    }, []);
     return (
         <div className="wrapper-form">
+            {redirect && <Redirect to={{
+                pathname: '/',
+            }}/>}
             <div className="container-form">
                 <h2>Zarejestruj się</h2>
                 <RegisterForm/>
